ee1ef15f3c434904760fdfd099601aac12cfb27e
hyunbin1 <kimgusqls1@gmail.com>
2022-01-16 15:28:42 +0900

    [add] JPQL 소개 및 작성

--- Jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ Jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -16,16 +17,18 @@ public class JpaMain {
 
         // 필요한 Entity Manager 코드 작성
         EntityTransaction transaction = entityManager.getTransaction(); // 트렌젝션 선언
+        transaction.begin(); // 트렌젝션 시작.
 
-        /* 1.
-        멤버 저장
-        try { // 오류를 대비하기 위해 try, catch문 사용.
-            transaction.begin(); // 트렌젝션 시작.
-            Member member = new Member();
-            member.setId(3L);
-            member.setName("HelloC");
+        //JPQL 회원 조회
+        try {
+            List<Member> result = entityManager.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(8)
+                    .getResultList(); // member Entity를 선택해서 조회한다.
 
-            entityManager.persist(member); // 객체를 영속성 컨텍스트에 저장. 그후 디비 저장.
+            for(Member member : result){
+                System.out.println("Member.name = " + member.getName());
+            }
 
             transaction.commit(); // 트렌젝션 종료 - 커밋 시점에 영속성 컨텍스트에 있는 내용이 DB에 저장된다.
         } catch(Exception e){
@@ -33,39 +36,8 @@ public class JpaMain {
         } finally{
             entityManager.close();
         }
-        */
 
-        /*
-        2. 멤버 조회
-        try { // 오류를 대비하기 위해 try, catch문 사용.
-            transaction.begin(); // 트렌젝션 시작.
 
-            Member findMember = entityManager.find(Member.class, 1L); // 클레스 이름, ID값을 넣어주면 찾는다
-            System.out.println("findMember.id = " + findMember.getId());
-            System.out.println("findMember.Name = " + findMember.getName());
-
-            transaction.commit(); // 트렌젝션 종료 - 커밋 시점에 영속성 컨텍스트에 있는 내용이 DB에 저장된다.
-        } catch(Exception e){
-            transaction.rollback();
-        } finally{
-            entityManager.close();
-        }
-        */
-
-//        3. 멤버 수정
-        try { // 오류를 대비하기 위해 try, catch문 사용.
-            transaction.begin(); // 트렌젝션 시작.
-
-            Member findMember = entityManager.find(Member.class, 1L); // 클레스 이름, ID값을 넣어주면 찾는다
-            findMember.setName("HelloJPA"); // 변경된 것이 있으면 jPA가 업데이트 쿼리를 작성해서 알아서 처리한다.
-            // 4. 멤버 삭제
-            // entityManager.remove(findMember);
-            transaction.commit(); // 트렌젝션 종료 - 커밋 시점에 영속성 컨텍스트에 있는 내용이 DB에 저장된다.
-        } catch(Exception e){
-            transaction.rollback();
-        } finally{
-            entityManager.close();
-        }
         entityManagerFactory.close();
     }
 }
